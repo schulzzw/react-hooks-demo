46c12936b2d0bed5a3ac43c300096b4be26fd9b1
Zachary Schulz <zschulz@wayfair.com>
2020-01-23 10:34:08 -0500

    update data

--- src/JobProvider.js
+++ src/JobProvider.js
@@ -6,55 +6,63 @@ class JobProvider extends React.Component {
   state = {
     jobs: [],
     applications: [],
-  }
+  };
 
   componentDidMount() {
     this.setState({
-      jobs: [{
-        id: 1,
-        title: 'Simple Scrubber',
-        pay: '$19/hr',
-        companyName: 'Steve Timber\'s Tub Masters',
-      },{
-        id: 2,
-        title: 'Trunk Flattener',
-        pay: '$33/hr',
-        companyName: 'Baby Got Back',
-      },{
-        id: 3,
-        title: 'Ranch Thinner',
-        pay: '$10/hr',
-        companyName: 'Hidden Field',
-      },{
-        id: 4,
-        title: 'Gauge Timer',
-        pay: '$55/hr',
-        companyName: 'Chernobyl No 4',
-      },{
-        id: 5,
-        title: 'Gumby Race Hiker',
-        pay: '$19/hr',
-        companyName: 'Block Heads Inc',
-      }]
-    })
+      jobs: [
+        {
+          id: 1,
+          title: 'Simple Scrubber',
+          pay: '$19/hr',
+          companyName: "Steve Timber's Tub Masters",
+        },
+        {
+          id: 2,
+          title: 'Trunk Flattener',
+          pay: '$33/hr',
+          companyName: 'Baby Got Back',
+        },
+        {
+          id: 3,
+          title: 'Ranch Thinner',
+          pay: '$10/hr',
+          companyName: 'Hidden Field',
+        },
+        {
+          id: 4,
+          title: 'Gauge Timer',
+          pay: '$55/hr',
+          companyName: 'Chernobyl No 5',
+        },
+        {
+          id: 5,
+          title: 'Gumby Race Hiker',
+          pay: '$19/hr',
+          companyName: 'Block Heads Inc',
+        },
+      ],
+    });
   }
 
   applyToJob = ({ jobId, candidateId }) => {
-    this.setState(prevState => ({
-      applications: [
-        ...prevState.applications,
-        {
-          jobId: this.state.jobs
-            .find(job => job.id===jobId).id,
-          candidateId,
-        }
-      ]
-    }), () => console.log('update applications', this.state.applications));
-  }
+    this.setState(
+      prevState => ({
+        applications: [
+          ...prevState.applications,
+          {
+            jobId: this.state.jobs.find(job => job.id === jobId).id,
+            candidateId,
+          },
+        ],
+      }),
+      () => console.log('update applications', this.state.applications)
+    );
+  };
 
-  viewCandidatesForJob = (jobId) => {
+  viewCandidatesForJob = jobId => {
     return this.state.applications.filter(app => app.jobId === jobId);
-  }
+  };
 
   render() {
     return (
@@ -67,11 +75,10 @@ class JobProvider extends React.Component {
       >
         {this.props.children}
       </JobContext.Provider>
-    )
+    );
   }
 }
 
 export default JobProvider;
 
 export { JobContext, JobProvider };
-
